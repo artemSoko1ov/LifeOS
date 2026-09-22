69ab1e9af9b7ab0e932059c58db64667c26883a4
artemSoko1ov <the.jelll.2006@gmail.com>
2026-09-22 17:02:34 +0500

    feat: add function api

---
+++ src/shared/api/api.ts
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:8080/api';
+
+export async function api(endpoint: string) {
+    try {
+        const response = await fetch(baseUrl + endpoint);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
