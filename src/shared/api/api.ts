03fd95039d8017ea5c1936590285d29e8a6798f9
artemSoko1ov <the.jelll.2006@gmail.com>
2026-09-25 14:21:31 +0500

    refactor: extend api client

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,9 +1,8 @@
 const baseUrl = 'http://localhost:8080/api';
 
-export async function api(endpoint: string) {
+export async function api(endpoint: string, options?: RequestInit) {
   try {
-    const response = await fetch(baseUrl + endpoint);
-
+    const response = await fetch(baseUrl + endpoint, options);
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
